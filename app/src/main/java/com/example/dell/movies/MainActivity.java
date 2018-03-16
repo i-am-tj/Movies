50a3c7f276db4793afcfe5f17bc35ba01d878512
itsmetj4u <itsmetanuj4u@gmail.com>
2018-03-16 19:34:52 +0530

    Stage One

--- app/src/main/java/com/example/dell/movies/MainActivity.java
+++ app/src/main/java/com/example/dell/movies/MainActivity.java
@@ -1,27 +1,126 @@
 package com.example.dell.movies;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private GridView mGridView;
+    private GridViewAdapter mGridAdapter;
+    private ArrayList<GridItem> mGridData;
+    private String FEED_URL = "http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=0bd231065ff59fa32eda34174f338c1e";
+    private ProgressBar mLoadingIndicator;
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        GridView gridview = (GridView) findViewById(R.id.gridview);
-        gridview.setAdapter(new ImageAdapter(this));
-
-        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View v,
-                                    int position, long id) {
-                Toast.makeText(MainActivity.this, "" + position, Toast.LENGTH_SHORT).show();
-            }
-        });
+        mLoadingIndicator = findViewById(R.id.mLoadingIndicator);
+        mGridView = findViewById(R.id.gridview);
+        mGridData = new ArrayList<>();
+        mGridAdapter = new GridViewAdapter(this, R.layout.grid_item_layout, mGridData);
+        mGridView.setAdapter(mGridAdapter);
+        new AsyncHttpTask().execute(FEED_URL);
     }
+
+    //Downloading data asynchronously
+    public class AsyncHttpTask extends AsyncTask<String, Void, Integer> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mLoadingIndicator.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected Integer doInBackground(String... params) {
+            Integer result = 0;
+            try {
+                // Create Apache HttpClient
+                HttpClient httpclient = new DefaultHttpClient();
+                HttpResponse httpResponse = httpclient.execute(new HttpGet(params[0]));
+                int statusCode = httpResponse.getStatusLine().getStatusCode();
+
+                // 200 represents HTTP OK
+                if (statusCode == 200) {
+                    String response = streamToString(httpResponse.getEntity().getContent());
+                    parseResult(response);
+                    result = 1; // Successful
+                } else {
+                    Log.d("Result:", result.toString());
+                    result = 0; //"Failed
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return result;
+        }
+        String streamToString(InputStream stream) throws IOException {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(stream));
+            String line;
+            String result = "";
+            while ((line = bufferedReader.readLine()) != null) {
+                Log.d("RESULT: ", result);
+                result += line;
+            }
+            
+            return result;
+        }
+        @Override
+        protected void onPostExecute(Integer result) {
+            mLoadingIndicator.setVisibility(View.GONE);
+            // Download complete. Let us update UI
+            if (result == 1) {
+                mGridAdapter.setGridData(mGridData);
+            } else {
+                Toast.makeText(MainActivity.this, "Failed to fetch data!", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    }
+
+    private void parseResult(String result) {
+
+        try {
+            JSONObject response = new JSONObject(result);
+            JSONArray array = response.getJSONArray("results");
+            GridItem item;
+            for (int i = 0; i < array.length(); i++) {
+                JSONObject imageobject = array.getJSONObject(i);
+                String image_url = imageobject.getString("backdrop_path");
+                item = new GridItem();
+                Log.d("URL:", image_url);
+                item.setImage("http://image.tmdb.org/t/p/w780"+image_url);
+                mGridData.add(item);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
 }
