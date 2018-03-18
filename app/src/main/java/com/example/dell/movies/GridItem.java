28c7d7632d718f17b8867c376c3d3b0dc910fa69
itsmetj4u <itsmetanuj4u@gmail.com>
2018-03-18 10:41:51 +0530

    stage One

---
+++ app/src/main/java/com/example/dell/movies/GridItem.java
@@ -0,0 +1,17 @@
+package com.example.dell.movies;
+
+/**
+ * Created by Dell on 16-03-2018.
+ */
+
+public class GridItem {
+    private String image;
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
